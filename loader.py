6c2778fec90e86f0a4d7e23aa3cd1b2019fa5905
hohlmann <hohlmann@hia.rwth-aachen.de>
2018-12-06 17:57:31 +0100

    modified for meditec

--- loader.py
+++ loader.py
@@ -9,31 +9,52 @@
 from data_generator.image import ImageDataGenerator
 import scipy.misc as misc
 import numpy as np
-import os, glob, itertools
+import os
+import glob
+import itertools
 from PIL import ImageFile
 from PIL import Image as pil_image
+import h5py
+import random
+
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
-# Modify this for data normalization 
+# Modify this for data normalization
 def preprocess(img, mean, std, label, normalize_label=True):
-    out_img = img / img.max() # scale to [0,1]
-    out_img = (out_img - np.array(mean).reshape(1,1,3)) / np.array(std).reshape(1,1,3) 
+    out_img = img / img.max()  # scale to [0,1]
+    out_img = (out_img - np.array(mean).reshape(1, 1, 3)) / \
+        np.array(std).reshape(1, 1, 3)
 
     if len(label.shape) == 4:
-        label = label[:,:,:,0]
+        label = label[:, :, :, 0]
     if normalize_label:
         if np.unique(label).size > 2:
-            print ('WRANING: the label has more than 2 classes. Set normalize_label to False')
-        label = label / label.max() # if the loaded label is binary has only [0,255], then we normalize it
+            print(
+                'WRANING: the label has more than 2 classes. Set normalize_label to False')
+        # if the loaded label is binary has only [0,255], then we normalize it
+        label = label / label.max()
     return out_img, label.astype(np.int32)
 
+
 def deprocess(img, mean, std, label):
-    out_img = img / img.max() # scale to [0,1]
-    out_img = (out_img * np.array(std).reshape(1,1,3)) + np.array(std).reshape(1,1,3) 
+    out_img = img / img.max()  # scale to [0,1]
+    out_img = (out_img * np.array(std).reshape(1, 1, 3)) + \
+        np.array(std).reshape(1, 1, 3)
     out_img = out_img * 255.0
 
     return out_img.astype(np.uint8), label.astype(np.uint8)
+
+# image normalization default: scale to [-1,1]
+
+
+def imerge(a, b, mean, std, normalize_label):
+    for img, label in itertools.zip_longest(a, b):
+        # j is the mask: 1) gray-scale and int8
+        img, label = preprocess(img, mean, std, label,
+                                normalize_label=normalize_label)
+        yield img, label
+
 
 '''
     Use the Keras data generators to load train and test
@@ -51,45 +72,146 @@ def deprocess(img, mean, std, label):
                 0/
 
 '''
-def dataLoader(path, batch_size, imSize, train_mode=True, mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5]):
-    # image normalization default: scale to [-1,1]
-    def imerge(a, b):
-        for img, label in itertools.zip_longest(a,b):
-            # j is the mask: 1) gray-scale and int8
-            img, label = preprocess(img, mean, std, label)
-            yield img, label
-    
+
+
+def dataLoader(path, batch_size, imSize, train_mode=True, mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]):
+
     # augmentation parms for the train generator
     if train_mode:
         train_data_gen_args = dict(
-                        horizontal_flip=True,
-                        vertical_flip=True,
-                        )
+            horizontal_flip=True,
+            vertical_flip=True,
+        )
     else:
         train_data_gen_args = dict()
-    
+
     # seed has to been set to synchronize img and mask generators
     seed = 1
     train_image_datagen = ImageDataGenerator(**train_data_gen_args).flow_from_directory(
-                                path+'img',
-                                class_mode=None,
-                                target_size=imSize,
-                                batch_size=batch_size,
-                                seed=seed,
-                                shuffle=train_mode)
+        path+'img',
+        class_mode=None,
+        target_size=imSize,
+        batch_size=batch_size,
+        seed=seed,
+        shuffle=train_mode)
     train_mask_datagen = ImageDataGenerator(**train_data_gen_args).flow_from_directory(
-                                path+'gt',
-                                class_mode=None,
-                                target_size=imSize,
-                                batch_size=batch_size,
-                                color_mode='grayscale',
-                                seed=seed,
-                                shuffle=train_mode)
-                                
+        path+'gt',
+        class_mode=None,
+        target_size=imSize,
+        batch_size=batch_size,
+        color_mode='grayscale',
+        seed=seed,
+        shuffle=train_mode)
+
     samples = train_image_datagen.samples
-    generator = imerge(train_image_datagen, train_mask_datagen)
+    generator = imerge(train_image_datagen,
+                       train_mask_datagen, mean, std, True)
 
     return generator, samples
 
 
-  
+def mergeDatasets(path):
+    list_us_array = []
+    list_gt_array = []
+
+    min_img_size = (266, 369)
+    num_rows_cut_bottom = 33
+
+   # max_num_zero_bottom_rows = 0
+
+    for f in sorted(os.listdir(path)):
+        # If f is directory, not a file
+        files_directory = os.path.join(path, f)
+        if not os.path.isdir(files_directory):
+            continue
+        print("entering directory: ", files_directory)
+        h5f = h5py.File(os.path.join(files_directory, 'us_gt_vol.h5'), 'r')
+        us_vol = h5f['us_vol'][:]
+        gt_vol = h5f['gt_vol'][:]
+        gt_vol = np.transpose(gt_vol, (1, 0, 2))
+
+        cut_at_ax0 = 0
+        cut_at_ax1 = 0
+        # To check maximum num of consecutive all 0.0 rows from bottom.
+        # for i in range(us_vol.shape[-1]):
+        #     sli = us_vol[:, :, i]
+        #     num_zero_bottom_rows = 0
+        #     for j in range(sli.shape[0]-1, 0, -1):
+        #         row = sli[j, :]
+        #         if np.all(row == 0.0):
+        #             num_zero_bottom_rows += 1
+        #         else:
+        #             break
+
+        #     if max_num_zero_bottom_rows < num_zero_bottom_rows:
+        #         max_num_zero_bottom_rows = num_zero_bottom_rows
+        # print(max_num_zero_bottom_rows)
+
+        if us_vol.shape[0] > min_img_size[0]:
+            cut_at_ax0 = random.randrange(
+                0, (us_vol.shape[0] - min_img_size[0]), 1)
+
+        if us_vol.shape[1] > min_img_size[1]:
+            cut_at_ax1 = random.randrange(
+                0, (us_vol.shape[1] - min_img_size[1]), 1)
+
+        us_vol = us_vol[cut_at_ax0:cut_at_ax0 +
+                        min_img_size[0] - num_rows_cut_bottom, cut_at_ax1:cut_at_ax1 + min_img_size[1], :]
+        gt_vol = gt_vol[cut_at_ax0:cut_at_ax0 +
+                        min_img_size[0] - num_rows_cut_bottom, cut_at_ax1:cut_at_ax1 + min_img_size[1], :]
+
+        list_us_array.append(us_vol)
+        list_gt_array.append(gt_vol)
+
+    X = np.dstack(list_us_array)
+    Y = np.dstack(list_gt_array)
+
+    return X, Y
+
+
+def dataLoaderNp(path, batch_size, train_mode=True, mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]):
+    X, Y = mergeDatasets(path)
+    X = X.reshape(X.shape[-1], X.shape[0], X.shape[1], 1)
+    Y = Y.reshape(Y.shape[-1], Y.shape[0], Y.shape[1], 1)
+    # X = np.expand_dims(X, -1)
+    # Y = np.expand_dims(Y, -1)
+    train_data_gen_args = dict()
+    # seed has to been set to synchronize img and mask generators
+    seed = 1
+    train_image_datagen = ImageDataGenerator(**train_data_gen_args).flow(
+        x=X,
+        batch_size=batch_size,
+        seed=seed,
+        shuffle=train_mode)
+    train_mask_datagen = ImageDataGenerator(**train_data_gen_args).flow(
+        x=Y,
+        batch_size=batch_size,
+        seed=seed,
+        shuffle=train_mode)
+
+    generator = imerge(train_image_datagen,
+                       train_mask_datagen, mean, std, False)
+
+    return generator, X.shape[0]
+
+
+if __name__ == "__main__":
+    X, Y = mergeDatasets(
+        "D:\\Users\\odgiiv.khuurkhunkhuu\\Datasets\\juliana_wo_symImages\\train")
+    print("X shape: ", X.shape)
+    print("Y shape: ", Y.shape)
+
+    X = X.transpose(-1, 0, 1)
+    Y = Y.transpose(-1, 0, 1)
+
+    print("X shape: ", X.shape)
+    print("Y shape: ", Y.shape)
+
+    nth = random.randrange(0, 100, 1)
+    print("Choosing image at {}th slice.".format(nth))
+
+    x = pil_image.fromarray(X[nth, :, :, ])
+    y = pil_image.fromarray(np.uint8(Y[nth, :, :] * 255), 'L')
+
+    x.show()
+    y.show()
--- utils.py
+++ utils.py
@@ -9,8 +9,12 @@
 # from keras import backend as K
 import numpy as np
 from PIL import Image
-import os, copy, shutil, json
+import os
+import copy
+import shutil
+import json
 from skimage import measure
+
 
 class VIS:
     def __init__(self, save_path):
@@ -32,42 +36,53 @@ class VIS:
 
     def save_seg(self, label_im, name, im=None, gt=None):
 
-        seg = Image.fromarray(label_im.astype(np.uint8), mode='P') # must convert to int8 first
+        # must convert to int8 first
+        seg = Image.fromarray(label_im.astype(np.uint8), mode='P')
         seg.palette = copy.copy(self.palette)
         if gt is not None or im is not None:
-            gt = Image.fromarray(gt.astype(np.uint8), mode='P') # must convert to int8 first]
+            # must convert to int8 first]
+            gt = Image.fromarray(gt.astype(np.uint8), mode='P')
             gt.palette = copy.copy(self.palette)
             im = Image.fromarray(im.astype(np.uint8), mode='RGB')
             I = Image.new('RGB', (label_im.shape[1]*3, label_im.shape[0]))
-            I.paste(im,(0,0))
-            I.paste(gt,(256,0))
-            I.paste(seg,(512,0))
+            I.paste(im, (0, 0))
+            I.paste(gt, (256, 0))
+            I.paste(seg, (512, 0))
             I.save(os.path.join(self.path, name))
-        else:   
-            seg.save(os.path.join(self.path, name))   
+        else:
+            seg.save(os.path.join(self.path, name))
 
     def add_sample(self, pred, gt):
         score_mean, score_cls = mean_IU(pred, gt)
         self.mean_iu.append(score_mean)
+
+        # if type(score_cls) is list and len(score_cls) == 2:
+        #     print(type(score_cls))
+
         self.cls_iu.append(score_cls)
-        
+
         return score_mean
 
     def compute_scores(self, suffix=0):
         meanIU = np.mean(np.array(self.mean_iu))
-        meanIU_per_cls = np.mean(np.array(self.cls_iu), axis=0)
-        print ('-'*20)
-        print ('overall mean IU: {} '.format(meanIU))
-        print ('mean IU per class')
-        for i, c in enumerate(meanIU_per_cls):
-            print ('\t class {}: {}'.format(i,c))
-        print ('-'*20)
-        
-        data = {'mean_IU': '%.2f' % (meanIU), 'mean_IU_cls': ['%.2f'%(a) for a in meanIU_per_cls.tolist()]}
-        self.score_history['%.10d' % suffix] = data
-        json.dump(self.score_history, open(os.path.join(self.path, 'meanIU{}.json'.format(self.suffix)),'w'), indent=2, sort_keys=True)
+        asarray = np.array(self.cls_iu)
+        # meanIU_per_cls = np.mean(asarray, axis=0)
+        print('-'*20)
+        print('overall mean IU: {} '.format(meanIU))
+        print('mean IU per class')
+        # for i, c in enumerate(meanIU_per_cls):
+        #     print('\t class {}: {}'.format(i, c))
+        print('-'*20)
 
-        
+        # data = {'mean_IU': '%.2f' % (meanIU), 'mean_IU_cls': [
+        #     '%.2f' % (a) for a in meanIU_per_cls.tolist()]}
+        data = {'mean_IU': '%.2f' % (meanIU)}
+
+        self.score_history['%.10d' % suffix] = data
+        json.dump(self.score_history, open(os.path.join(
+            self.path, 'meanIU{}.json'.format(self.suffix)), 'w'), indent=2, sort_keys=True)
+
+
 def mean_IU(eval_segm, gt_segm):
     '''
     (1/n_cl) * sum_i(n_ii / (t_i + sum_j(n_ji) - n_ii))
@@ -75,7 +90,7 @@ def mean_IU(eval_segm, gt_segm):
 
     check_size(eval_segm, gt_segm)
 
-    cl, n_cl   = union_classes(eval_segm, gt_segm)
+    cl, n_cl = union_classes(eval_segm, gt_segm)
     _, n_cl_gt = extract_classes(gt_segm)
     eval_mask, gt_mask = extract_both_masks(eval_segm, gt_segm, cl, n_cl)
 
@@ -84,21 +99,23 @@ def mean_IU(eval_segm, gt_segm):
     for i, c in enumerate(cl):
         curr_eval_mask = eval_mask[i, :, :]
         curr_gt_mask = gt_mask[i, :, :]
- 
+
         if (np.sum(curr_eval_mask) == 0) or (np.sum(curr_gt_mask) == 0):
             continue
 
         n_ii = np.sum(np.logical_and(curr_eval_mask, curr_gt_mask))
-        t_i  = np.sum(curr_gt_mask)
+        t_i = np.sum(curr_gt_mask)
         n_ij = np.sum(curr_eval_mask)
 
         IU[i] = n_ii / (t_i + n_ij - n_ii)
- 
+
     mean_IU_ = np.sum(IU) / n_cl_gt
     return mean_IU_, IU
 
 
 '''Used by Tensorflow'''
+
+
 def dice_coef(y_true, y_pred):
     smooth = 1.
     y_true_f = K.flatten(y_true)
@@ -106,14 +123,17 @@ def dice_coef(y_true, y_pred):
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
+
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
 
+
 def extract_both_masks(eval_segm, gt_segm, cl, n_cl):
     eval_mask = extract_masks(eval_segm, cl, n_cl)
-    gt_mask   = extract_masks(gt_segm, cl, n_cl)
+    gt_mask = extract_masks(gt_segm, cl, n_cl)
 
     return eval_mask, gt_mask
+
 
 def extract_classes(segm):
     cl = np.unique(segm)
@@ -121,17 +141,19 @@ def extract_classes(segm):
 
     return cl, n_cl
 
+
 def union_classes(eval_segm, gt_segm):
     eval_cl, _ = extract_classes(eval_segm)
-    gt_cl, _   = extract_classes(gt_segm)
+    gt_cl, _ = extract_classes(gt_segm)
 
     cl = np.union1d(eval_cl, gt_cl)
     n_cl = len(cl)
 
     return cl, n_cl
 
+
 def extract_masks(segm, cl, n_cl):
-    h, w  = segm_size(segm)
+    h, w = segm_size(segm)
     masks = np.zeros((n_cl, h, w))
 
     for i, c in enumerate(cl):
@@ -139,14 +161,16 @@ def extract_masks(segm, cl, n_cl):
 
     return masks
 
+
 def segm_size(segm):
     try:
         height = segm.shape[0]
-        width  = segm.shape[1]
+        width = segm.shape[1]
     except IndexError:
         raise
 
     return height, width
+
 
 def check_size(eval_segm, gt_segm):
     h_e, w_e = segm_size(eval_segm)
